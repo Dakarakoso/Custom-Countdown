be8226d5f167766e3badfb8e3ef511de6ec7f7aa
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2021-09-22 03:23:17 +0900

    added an event listener for the form

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
+
+let countdownTitle = "";
+let countdownDate = "";
+
+// set date input min with current date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+// take the value from form
+const updateCountdown = (e) => {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countdownDate);
+};
+
+// event listener
+countdownForm.addEventListener("submit", updateCountdown);
